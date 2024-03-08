fba7223632bcaaa674a0c9f36abdaa20ce87de72
OGOZ111 <djsmallboy@gmail.com>
2024-03-08 11:00:34 +0200

    first commit

---
+++ src/app/countries/CountriesDetail.tsx
@@ -0,0 +1,19 @@
+const CountriesDetail = () => {
+    
+    interface Country {
+        name: string;
+        capital: string;
+        population: number;
+        area: number;
+        flag: string;
+    }
+
+    return (
+
+        <div>
+            <h1>Country Detail</h1>
+        </div>
+
+    )
+}   
+export default CountriesDetail;
