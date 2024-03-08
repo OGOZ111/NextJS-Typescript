6add7ca8be083817bd52d0f9b306ad655312171e
OGOZ111 <djsmallboy@gmail.com>
2024-03-08 12:44:33 +0200

    defined ts types from API

--- src/app/countries/CountriesAPI.tsx
+++ src/app/countries/CountriesAPI.tsx
@@ -4,9 +4,15 @@ import axios from "axios"
 import React, { useEffect, useState } from "react"
 
 
+interface CountryCapital {
+    capital: string[];
+
+}
+
+
 const CountriesAPI = () => {
 
-    const [countries, setCountries] = useState([])
+    const [countries, setCountries] = useState<CountryCapital[]>([])
 
     const getCountries = async () => {
         const response = await axios.get("https://restcountries.com/v3.1/all?fields=capital")
@@ -22,10 +28,22 @@ const CountriesAPI = () => {
 
 
     return (
+    <>
+
+
         <div>
             <h1>Countries API here</h1>
-
+            {countries.map((country) => (
+                <div key={country.capital[0]}>
+                    <h2>{country.capital[0]}</h2>
+                </div>  
+            ))
+                    
+                
+            
+            } 
         </div>
+        </>
     );
 }
 
